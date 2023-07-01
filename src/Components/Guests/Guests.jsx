0c23bad73576d39fec1178a252df75df20d37e3e
belousovigorvic <68902284+belousovigorvic@users.noreply.github.com>
2023-07-01 17:00:16 +0600

    Доработал слайдер

--- src/Components/Guests/Guests.jsx
+++ src/Components/Guests/Guests.jsx
@@ -4,7 +4,6 @@ import person1 from '../../asserts/Guest-img/person-1.jpg'
 import person2 from '../../asserts/Guest-img/person-2.jpg'
 import person3 from '../../asserts/Guest-img/person-3.jpg'
 
-
 import { useEffect, useState } from "react";
 
 const guests = [
@@ -12,26 +11,23 @@ const guests = [
         id: 1,
         pic: person1,
         name: 'Николай',
-        review: 'Мой День рождения был незабываемым благодаря этому ресторану! Я прочувствовал атмосферу родной Армении в каждой детали. Обслуживание было на высшем уровне, а особенно порадовал комплимент в виде фруктовой тарелки. Этот ресторан стоит рекомендовать всем моим друзьям и родственникам, особенно тем, кто путешествует в Санкт-Петербург!'
     },
     {
         id: 2,
         pic: person2,
         name: 'Антон',
-        review: 'Посещение ресторана армянской кухни было настоящим кулинарным путешествием! Мы отведали такие блюда, которые раньше никогда не пробовали, и были приятно удивлены их вкусом и оригинальностью. Особенно запомнились мясные шашлыки и армянский плов. Обслуживание было внимательным, а интерьер ресторана - уютным и аутентичным.'
     },
     {
         id: 3,
         pic: person3,
         name: 'Анна',
-        review: 'Я посетила этот ресторан недавно и осталась очень довольной. Обстановка была приятной и уютной, а обслуживание безупречным. Особенно впечатлило качество еды - каждое блюдо было свежим, вкусным и готовлено с большим мастерством. Рекомендую этот ресторан всем, кто ищет отличное место для приятного ужина или обеда.'
     },
 ]
 
 const Guests = () => {
     const [data, setData] = useState([]);
     const [activeIndex, setActiveIndex] = useState(0);
-    
+
     const objLength = guests.length - 1;
     let intervalId;
 
@@ -64,23 +60,28 @@ const Guests = () => {
         };
     }, []);
 
-
     return (
         <div className={style.wrapper}>
             <div className={`${style.mobile_container} container`}>
                 <div className={style.guests}>
                     {data && (<p className={style.text}>{data[activeIndex]?.body}</p>)}
-                    <img
-                        className={`${style.guests__pic} ${style.guests__picActive}`}
-                        src={guests[activeIndex].pic}
-                        alt={`person${activeIndex + 1}`}
-                    />
-                    <p className={style.guest}>Посетитель</p>
-                    <p className={style.guests__name}>{guests[activeIndex].name}</p>
-                    <div className={style.pagination}>
-                        <div onClick={() => slider(0)} className={`${style.pagination__item} ${activeIndex === 0 ? style.pagination__itemActive : ''}`}></div>
-                        <div onClick={() => slider(1)} className={`${style.pagination__item} ${activeIndex === 1 ? style.pagination__itemActive : ''}`}></div>
-                        <div onClick={() => slider(2)} className={`${style.pagination__item} ${activeIndex === 2 ? style.pagination__itemActive : ''}`}></div>
+                    <div className={style.wrap}>
+                        {guests.map((guest, index) => (
+                            <img
+                                key={guest.id}
+                                className={`${style.guests__pic} ${activeIndex === index ? style.guests__picActive : ''
+                                    }`}
+                                src={guest.pic}
+                                alt={`person${index + 1}`}
+                            />
+                        ))}
+                        <p className={style.guest}>Посетитель</p>
+                        <p className={style.guests__name}>{guests[activeIndex].name}</p>
+                        <div className={style.pagination}>
+                            <div onClick={() => slider(0)} className={`${style.pagination__item} ${activeIndex === 0 ? style.pagination__itemActive : ''}`}></div>
+                            <div onClick={() => slider(1)} className={`${style.pagination__item} ${activeIndex === 1 ? style.pagination__itemActive : ''}`}></div>
+                            <div onClick={() => slider(2)} className={`${style.pagination__item} ${activeIndex === 2 ? style.pagination__itemActive : ''}`}></div>
+                        </div>
                     </div>
                 </div>
             </div>
