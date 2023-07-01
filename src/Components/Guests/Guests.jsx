c51176eaf22978c069e5e4db640f06253e45b568
belousovigorvic <68902284+belousovigorvic@users.noreply.github.com>
2023-07-01 14:15:34 +0600

    Добавил автослайдер

--- src/Components/Guests/Guests.jsx
+++ src/Components/Guests/Guests.jsx
@@ -5,7 +5,7 @@ import person2 from '../../asserts/Guest-img/person-2.jpg'
 import person3 from '../../asserts/Guest-img/person-3.jpg'
 
 
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 const guests = [
     {
@@ -24,49 +24,65 @@ const guests = [
         id: 3,
         pic: person3,
         name: 'Анна',
-        review: 'Я посетил этот ресторан недавно и остался очень довольным. Обстановка была приятной и уютной, а обслуживание безупречным. Особенно впечатлило качество еды - каждое блюдо было свежим, вкусным и готовлено с большим мастерством. Рекомендую этот ресторан всем, кто ищет отличное место для приятного ужина или обеда.'
+        review: 'Я посетила этот ресторан недавно и осталась очень довольной. Обстановка была приятной и уютной, а обслуживание безупречным. Особенно впечатлило качество еды - каждое блюдо было свежим, вкусным и готовлено с большим мастерством. Рекомендую этот ресторан всем, кто ищет отличное место для приятного ужина или обеда.'
     },
 ]
 
 const Guests = () => {
-    const [data, setData] = useState()
-    const [activeIndex, setActiveIndex] = useState(0)
+    const [data, setData] = useState([]);
+    const [activeIndex, setActiveIndex] = useState(0);
+    
+    const objLength = guests.length - 1;
+    let intervalId;
 
     const fetchData = async () => {
         try {
-            const data = await fetch('https://jsonplaceholder.typicode.com/comments')
-            const response = await data.json()
-            const sliceResponse = response.slice(0, 3)
-            setData(sliceResponse)
+            const response = await fetch('https://jsonplaceholder.typicode.com/comments');
+            const responseData = await response.json();
+            const sliceResponse = responseData.slice(0, 3);
+            setData(sliceResponse);
         } catch (error) {
             console.error('Произошла ошибка:', error.message);
         }
-    }
-
-    useEffect(() => {
-        fetchData()
-    }, [])
+    };
 
     const slider = (index) => {
         setActiveIndex(index);
-    }
+    };
+
+    const autoSlider = () => {
+        intervalId = setInterval(() => {
+            setActiveIndex(prevIndex => prevIndex === objLength ? 0 : prevIndex + 1);
+        }, 6000);
+    };
+
+    useEffect(() => {
+        fetchData();
+        autoSlider();
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
 
     return (
         <div className={style.wrapper}>
             <div className={`${style.mobile_container} container`}>
-                    <div className={style.guests}>
-                        {data && (
-                            <p className={style.text}>{data[activeIndex]?.body}</p>
-                        )}
-                        <img className={style.guests__pic} src={guests[activeIndex].pic} alt="person1"/>
-                        <p className={style.guest}>Посетитель</p>
-                        <p className={style.guests__name}>{guests[activeIndex].name}</p>
-                        <div className={style.pagination}>
-                            <div onClick={() => slider(0)} className={`${style.pagination__item} ${activeIndex === 0 ? style.pagination__itemActive : ''}`}></div>
-                            <div onClick={() => slider(1)} className={`${style.pagination__item} ${activeIndex === 1 ? style.pagination__itemActive : ''}`}></div>
-                            <div onClick={() => slider(2)} className={`${style.pagination__item} ${activeIndex === 2 ? style.pagination__itemActive : ''}`}></div>
-                        </div>
+                <div className={style.guests}>
+                    {data && (<p className={style.text}>{data[activeIndex]?.body}</p>)}
+                    <img
+                        className={`${style.guests__pic} ${style.guests__picActive}`}
+                        src={guests[activeIndex].pic}
+                        alt={`person${activeIndex + 1}`}
+                    />
+                    <p className={style.guest}>Посетитель</p>
+                    <p className={style.guests__name}>{guests[activeIndex].name}</p>
+                    <div className={style.pagination}>
+                        <div onClick={() => slider(0)} className={`${style.pagination__item} ${activeIndex === 0 ? style.pagination__itemActive : ''}`}></div>
+                        <div onClick={() => slider(1)} className={`${style.pagination__item} ${activeIndex === 1 ? style.pagination__itemActive : ''}`}></div>
+                        <div onClick={() => slider(2)} className={`${style.pagination__item} ${activeIndex === 2 ? style.pagination__itemActive : ''}`}></div>
                     </div>
+                </div>
             </div>
         </div>
     )
