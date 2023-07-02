8538ce122545df022ab03774b075325fed38fa86
Mayan-Muhamed <muhamed.mayan28@gmail.com>
2023-07-02 20:49:30 +0600

    muha

--- src/Components/Reservation/Reservation.jsx
+++ src/Components/Reservation/Reservation.jsx
@@ -1,6 +1,25 @@
-import React from "react"
+import classes from './Reservation.module.css';
+import Button from '../UI/Button.jsx';
+import React from 'react';
+
+
+
 const Reservation = () => {
-    return (<></>)
+     return(
+          <div className={classes.reservation}>
+               <div className='container'>
+                    <div className={classes.reservationInner}>
+                         <div className={classes.reservationText}>
+                              <h3 className={classes.reservationTitle}>Отпразднуйте в одном из самых лучших ресторанов.</h3>
+                              <p className={classes.reservationDesc}>Только в этом месяце бизнес-ланч от 250 ₽</p>
+                         </div>
+                         <div className={classes.reservationButtonBlock}>
+                              <Button textButton={"ЗАКАЗ СТОЛИКА"} w={157} h={47} bg={"#FF7400"} />
+                         </div>
+                    </div>
+               </div>
+          </div>
+     )
 }
 
 export default Reservation
--- src/Components/Story/Story.jsx
+++ src/Components/Story/Story.jsx
@@ -1,11 +1,42 @@
-import classes from "./Story.module.css";
-import dishes from "../../asserts/dihes.png"
+import classes from './Story.module.css';
+import dishes from '../../asserts/dihes.png';
 
 const Story = () => {
-    return (
-        <div>
-
-        </div>
-    )
-}
-export default Story
+     return (
+          <div className={classes.story}>
+               <div className='container'>
+                    <div className={classes.storyInner}>
+                         <div className={classes.storyImageBlock}>
+                              <img src={dishes} alt='#'  className={classes.storyImage}/>
+                         </div>
+                         <div className={classes.storyTextBlock}>
+                              <div className={classes.storyLeftBlock}>
+                                   <h3 className={classes.storyTitle}>Наша <span className={classes.storyOrange}>История</span></h3>
+                                   <p className={classes.storyDesc}>Как и у любого другого самобытного места, у нас есть своя, особая история.
+                                        Идея ресторана пришла основателям
+                                        неожиданно. Во время прогулки по лесу создатель нашего
+                                        ресторана застрял в сотнях километров от ближайшего населенного пункта.
+                                        Вдали от цивилизации и связи им пришлось навремя обустровать
+                                        себе нехитрый быт, добывать и готовить себе еду.</p>
+                              </div>
+                              <div className={classes.storyTextDishes}>
+                                   <div className={classes.storyText}>
+                                        <span className={classes.storyNumber}>93<br /></span>
+                                        <span className={classes.storyDishes}>Напитки</span>
+                                   </div>
+                                   <div className={classes.storyText}>
+                                        <span  className={classes.storyNumber}>206<br /></span>
+                                        <span className={classes.storyDishes}>Еда</span>
+                                   </div>
+                                   <div className={classes.storyText}>
+                                        <span  className={classes.storyNumber}>71<br /></span>
+                                        <span className={classes.storyDishes}>Закуски</span>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     );
+};
+export default Story;
