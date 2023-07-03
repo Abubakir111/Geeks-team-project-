ffc45ce4a5a558e577d6815ebb0661929bcc1c91
Asema00 <130225611+Asema00@users.noreply.github.com>
2023-07-03 15:38:29 +0600

    komit

--- src/Components/Cookers/Cookers.jsx
+++ src/Components/Cookers/Cookers.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import styles from '../../asserts/Cookers_module.css';
+import cooker1 from '../../img cook/cog.png'
+import cooker2 from '../../img cook/Mask Group (1).png'
+import cooker3 from '../../img cook/image 4.png'
 
-const Cookers = () => {
-    return (<>
-    sdsdsdsdsdsd
-    </>)
+import Title from "../UI/Title";
+const Cookers = () =>
+{
+    const images = [cooker1, cooker2, cooker3]
+    return (
+        <div className={styles.wrapper}>
+                <Title title={'Галерея'} item={'Блюд'} />
+                <div className={styles.cookers}>
+                    {
+                        images.map((cooker, i) => <div className={styles.cookersCard} key={i}><img src={cooker} alt="img" /></div>)
+                    }
+                </div>
+
+        </div>
+    )
 }
 export default Cookers
+
