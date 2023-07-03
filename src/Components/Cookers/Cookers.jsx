3aeb5308194404da77c3865e5132be02c64ac058
Asema00 <130225611+Asema00@users.noreply.github.com>
2023-07-04 00:14:14 +0600

    commit

--- src/Components/Cookers/Cookers.jsx
+++ src/Components/Cookers/Cookers.jsx
@@ -1,23 +1,25 @@
 import React from "react";
-import styles from '../../asserts/Cookers_module.css';
-import cooker1 from '../../img cook/cog.png'
-import cooker2 from '../../img cook/Mask Group (1).png'
-import cooker3 from '../../img cook/image 4.png'
-
+import styles from './Cookers.module.css';
+import cooker1 from '../../asserts/Cooker/cooker1.jpg'
+import cooker2 from '../../asserts/Cooker/cooker2.jpg'
+import cooker3 from '../../asserts/Cooker/cooker3.jpg'
+import baner from '../../asserts/Cooker/baner.jpg'
 import Title from "../UI/Title";
 const Cookers = () =>
 {
     const images = [cooker1, cooker2, cooker3]
     return (
         <div className={styles.wrapper}>
-                <Title title={'Галерея'} item={'Блюд'} />
+            <div className={`${styles.mobile_container} container`}>
+                <Title title={'Наши'} item={'Повара'} />
                 <div className={styles.cookers}>
                     {
                         images.map((cooker, i) => <div className={styles.cookersCard} key={i}><img src={cooker} alt="img" /></div>)
                     }
                 </div>
+            </div>
+            <div className={styles.baner}><img src={baner} alt="img" /></div>
         </div>
     )
 }
 export default Cookers
-
