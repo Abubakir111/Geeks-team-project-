03c8f3a1f0cdef00e64264f8a8eaa3148b7ca356
dalibi120707 <dsadygulova183@gmail.com>
2023-07-04 19:43:47 +0600

    верстка и стиль,скоро буду делать логику

--- src/Components/ourDishes/OurDishes.jsx
+++ src/Components/ourDishes/OurDishes.jsx
@@ -1,12 +1,41 @@
 import React from "react"
+import OurDishesStyle from '../OurDishes/OurDishesStyle.module.css'
+import Burger from '../../asserts/img-our-dishes/burger.jpg'
+import Pizza from '../../asserts/img-our-dishes/pizza.jpg'
 
 const OurDishes = () => {
     return (<>
-    Я Далия и точка !
-    Я Далия и точка !
-    Я Далия и точка !
-    йцукенгшлотрпав
-    Я Далия и точка !
+        <div className="Dishes">
+            <div className="dishes_title">
+                Наши <span>Блюда</span>
+            </div>
+            <div className={OurDishesStyle.OurDishesWrapper}>
+                <img className={OurDishesStyle.OurDishesImage} src={Pizza} alt="there pizza" />
+            </div>
+
+            <div className="burger_items">
+                <div className="burger_item">
+                    <img className={OurDishesStyle.OurDishesImage} src={Burger} alt="there burger" />
+                </div>
+                <div className="burger_text">
+                    Гамбургер мини -------------- 220 ₽
+                </div>
+
+                <div className="burger_item">
+                    <img className={OurDishesStyle.OurDishesImage} src={Burger} alt="there burger" />
+                    <div className="burger_text">
+                        Гамбургер мини -------------- 220 ₽
+                    </div>
+                </div>
+
+                <div className="burger_item">
+                    <img className={OurDishesStyle.OurDishesImage} src={Burger} alt="there burger" />
+                    <div className="bureger_text">
+                        Гамбургер мини -------------- 220 ₽
+                    </div>
+                </div>
+            </div>
+        </div>
     </>)
 }
 
