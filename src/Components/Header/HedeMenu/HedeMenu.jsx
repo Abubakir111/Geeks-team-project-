a9dea6544c767fbee682c7bb993b459ab91b03c5
Bakir <bakirbazarkul16@gmail.com>
2023-07-05 23:20:09 +0300

    Добавить картинки для меню

--- src/Components/Header/HedeMenu/HedeMenu.jsx
+++ src/Components/Header/HedeMenu/HedeMenu.jsx
@@ -1,36 +1,13 @@
 import menuStyle from './HedeMenuStyle.module.css'
-import pizza from '../../../asserts/Heder_img/hederMenu_images/pizza.png'
-import beverages from '../../../asserts/Heder_img/hederMenu_images/napitki.png'
-import burger from '../../../asserts/Heder_img/hederMenu_images/burger.png'
-
+import Product from './Product/Product'
 const HedeMenu = ({ Toggles }) => {
-    const tovar = [
-        { title: 'Питца' },
-        {
-            img: pizza,
-            item: "Питца пеперони ",
-            prace: 300 + 'сом'
-        }
 
-    ]
     return (<div className={menuStyle.container}>
 
-        <div className={menuStyle.menu_container}>Меню</div>
+        <div className={menuStyle.title}>ALEMOND</div>
         <button onClick={Toggles} className={menuStyle.closBtn}>&#x2717;</button>
-        <div className={menuStyle.menuBlock}>
-            {
-                tovar.map(el =>
-                    <div  >
-                        <div >
-                            <ul>
-                                <ul >{el.title}</ul>
-                                <li>{el.item} </li>
-                            </ul>
-                            <img className={menuStyle.img} src={el.img} />
-                            sdsdsdfghkls;ci
-                        </div>
-                    </div>)
-            }
+        <div >
+            <Product />
         </div>
     </div>)
 }
