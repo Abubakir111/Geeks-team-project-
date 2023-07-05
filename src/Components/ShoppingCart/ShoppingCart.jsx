845bae1612ce16ceef7df9619570d68e193adc57
belousovigorvic <belousovigorvic94@gmail.com>
2023-07-05 14:57:11 +0600

    Сделал вывод картинок

--- src/Components/ShoppingCart/ShoppingCart.jsx
+++ src/Components/ShoppingCart/ShoppingCart.jsx
@@ -16,11 +16,13 @@ const ShoppingCart = ({close}) => {
     const [selectedProduct, setSelectedProduct] = useState([]);
     const [price, setPrice] = useState();
     const [allCategoryProduct, setAllCategoryProduct] = useState();
-    const [confirm, setConfirm] = useState(false)
+    const [confirm, setConfirm] = useState(false);
+
+    const [getId, setGetId] = useState();
 
     const getConfirm = () => {
-        selectedProduct.length !== 0  ? setConfirm(true) : null
-    }
+        selectedProduct.length !== 0 ? setConfirm(true) : null;
+    };
 
     useEffect(() => {
         setData([...Menu]);
@@ -46,11 +48,10 @@ const ShoppingCart = ({close}) => {
 
     const handleSearch = () => {
         const searchArray = data.filter(item =>
-            item.name.toLowerCase().includes(searchValue.toLowerCase()),
+            item.name.toLowerCase().includes(searchValue.toLowerCase())
         );
         setFilterCategory(searchArray);
     };
-
 
     const handleChange = (event) => {
         setSearchValue(event.target.value);
@@ -80,18 +81,39 @@ const ShoppingCart = ({close}) => {
         handleSearch();
     }, [searchValue]);
 
+    const [showTooltip, setShowTooltip] = useState(false);
+
+    const handleMouseEnter = () => {
+        setShowTooltip(true);
+    };
+
+    const handleMouseLeave = () => {
+        setShowTooltip(false);
+    };
+
+    const getIndexImg = (index) => {
+        setGetId(index);
+    };
+
     return (
         <div>
-            { confirm  ? <TableModal close={close} name={'Сделать Заказ!'} onClick={close}/> :
+            {confirm ? (
+                <TableModal close={close} name={'Сделать Заказ!'} onClick={close}/>
+            ) : (
                 <div>
                     <div className={styles.cart}>
-                        <img onClick={close} className={styles.cart__exit} src={cross} alt='cross'/>
-                        <img src={logo} alt='logo'/>
+                        <img
+                            onClick={close}
+                            className={styles.cart__exit}
+                            src={cross}
+                            alt="cross"
+                        />
+                        <img src={logo} alt="logo"/>
                         <h2 className={styles.cart__title}>Доставка</h2>
                         <input
                             className={styles.cart__input}
-                            placeholder='Найти еду!'
-                            type='text'
+                            placeholder="Найти еду!"
+                            type="text"
                             onChange={handleChange}
                             value={searchValue}
                         />
@@ -100,32 +122,66 @@ const ShoppingCart = ({close}) => {
                                 <button
                                     key={item}
                                     onClick={() => handleClick(item)}
-                                    className={`${styles.cart__categoryWrap} ${activeGroup === item ? styles.activeGroup : ''}`}>
+                                    className={`${styles.cart__categoryWrap} ${activeGroup === item ? styles.activeGroup : ''
+                                    }`}
+                                >
                                     <p className={styles.cart__categoryItem}>{item}</p>
                                 </button>
                             ))}
                             <button
-                                className={`${styles.cart__categoryWrap} ${allCategoryProduct === 'Все продукты' ? styles.activeGroup : ''}`}
+                                className={`${styles.cart__categoryWrap} ${allCategoryProduct === 'Все продукты'
+                                    ? styles.activeGroup
+                                    : ''
+                                }`}
                                 onClick={(event) => {
                                     setAllCategoryProduct(event.target.innerText);
                                     setFilterCategory(data);
                                     setActiveGroup('0');
-                                }}>Все продукты
+                                }}
+                            >
+                                Все продукты
                             </button>
                         </div>
                         <div className={styles.cart__list}>
                             <ul className={styles.cart__ul}>
-                                {filterCategory.length === 0 ?
-                                    <h3 style={{marginBottom: '20px'}}>Ничего не
-                                        найдено!</h3> : filterCategory.map(item => (
+                                {filterCategory.length === 0 ? (
+                                    <p style={{marginBottom: '20px'}}>Ничего не найдено! &#128561;</p>
+                                ) : (
+                                    filterCategory.map((item, index) => (
                                         <li key={item.name} className={styles.cart__li}>
-                                            <span className={styles.name}>{item.name}</span> <span
-                                            className={styles.rightBLock}>{item.price} сом <span><img
-                                            onClick={() => {
-                                                addProduct(item);
-                                            }} className={styles.plus} src={plus} alt='plus'/></span></span>
+                                            <span
+                                                onMouseEnter={() => {
+                                                    handleMouseEnter();
+                                                    getIndexImg(index);
+                                                }}
+                                                onMouseLeave={handleMouseLeave}
+                                                className={styles.name}
+                                            >
+                                                {item.name}
+                                            </span>{' '}
+                                            <span className={styles.rightBLock}>
+                                                {item.price} сом{' '}
+                                                <span>
+                                                    <img
+                                                        onClick={() => {
+                                                            addProduct(item);
+                                                        }}
+                                                        className={styles.plus}
+                                                        src={plus}
+                                                        alt="plus"
+                                                    />
+                                                </span>
+                                            </span>
                                         </li>
-                                    ))}
+                                    ))
+                                )}
+                                {showTooltip && (
+                                    <img
+                                        className={styles.modalPic}
+                                        src={filterCategory[getId]?.img}
+                                        alt="img"
+                                    />
+                                )}
                             </ul>
                         </div>
                         <div className={styles.cart__selected}>
@@ -138,7 +194,15 @@ const ShoppingCart = ({close}) => {
                                         <li key={index}>
                                             <div className={styles.selectedWrap}>
                                                 <span>{item.name}</span>
-                                                <span>{item.price} сом <img onClick={() => deleteProduct(index)} className={styles.cross} src={cross} alt='cross'/></span>
+                                                <span>
+                                                    {item.price} сом{' '}
+                                                    <img
+                                                        onClick={() => deleteProduct(index)}
+                                                        className={styles.cross}
+                                                        src={cross}
+                                                        alt="cross"
+                                                    />
+                                                </span>
                                             </div>
                                         </li>
                                     ))
@@ -148,16 +212,29 @@ const ShoppingCart = ({close}) => {
                         <div className={styles.totalPrice}>
                             <div className={styles.totalPriceWrap}>
                                 <div className={styles.btnWrap}>
-                                    <Button onClick={getConfirm} textButton={selectedProduct.length === 0 ? 'Закажи что нибудь' : 'Заказать Доставку'} w={157} h={47} bg={'#FF7400'}/>
+                                    <Button
+                                        onClick={getConfirm}
+                                        textButton={
+                                            selectedProduct.length === 0
+                                                ? 'Закажи что-нибудь'
+                                                : 'Заказать Доставку'
+                                        }
+                                        w={157}
+                                        h={47}
+                                        bg={'#FF7400'}
+                                    />
                                 </div>
                                 <div className={styles.totalPriceWrapper}>
-                                    <span style={{fontWeight: '600'}} className={styles.totalPriceItem}>Итого: {price} сом</span>
+                                    <span style={{fontWeight: '600'}} className={styles.totalPriceItem}>
+                                        Итого: {price} сом
+                                    </span>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <Overlay exit={close}/>
-                </div>}
+                </div>
+            )}
         </div>
     );
 };
