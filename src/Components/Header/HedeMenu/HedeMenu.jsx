5763760ea1e767438a9e3ef0f0028b3f08387e43
Bakir <bakirbazarkul16@gmail.com>
2023-07-06 23:38:12 +0300

    Сделал  пол работы  модального окна меню1

--- src/Components/Header/HedeMenu/HedeMenu.jsx
+++ src/Components/Header/HedeMenu/HedeMenu.jsx
@@ -1,10 +1,10 @@
 import menuStyle from './HedeMenuStyle.module.css'
 import Product from './Product/Product'
+import MenuFon from '../../../asserts/Heder_img/hederMenu_images/menuFon7.jpg'
 const HedeMenu = ({ Toggles }) => {
 
     return (<div className={menuStyle.container}>
-
-        <div className={menuStyle.title}>ALEMOND</div>
+        <img className={menuStyle.fonimage} src={MenuFon} alt="" srcset="" />
         <button onClick={Toggles} className={menuStyle.closBtn}>&#x2717;</button>
         <div >
             <Product />
